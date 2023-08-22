02304bf787a38485dca591d538965032c99cc984
Obzoriks <said.turdiev228@gmail.com>
2023-08-22 21:11:45 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+
+function showText(imageNumber) {
+    document.getElementById("text" + imageNumber).style.display = "block";
+}
+
+function hideText(imageNumber) {
+    document.getElementById("text" + imageNumber).style.display = "none";
+}
+
